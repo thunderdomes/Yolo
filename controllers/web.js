52883a92463fc281e9fb2195fa6f38a51a4ad7f3
Arie <arie@wego.com>
2013-06-24 00:44:51 +0700

    add function to read markdown

--- controllers/web.js
+++ controllers/web.js
@@ -1,8 +1,18 @@
+var fs = require("fs");
+var marked = require("marked"); // https://github.com/chjj/marked
 web = {
 	index : function(req, res, next){
-		 res.render('pages/index.ejs',{
-				title:"merdeka",		 	
+		fs.readFile("./content/tes.md", "ascii", function (err, data) {
+			if (err) {
+				throw err;
+			}
+			res.render('pages/index.ejs',{
+				title:"merdeka",
+				content:marked(data),
+
 		 });
+		});
+		 
 	},
 
 }

ea2ab18a35dd93a8092cb3f4787c7714b75a9951
Arie <arie@wego.com>
2013-06-25 19:14:21 +0700

    update landing page

--- controllers/web.js
+++ controllers/web.js
@@ -2,15 +2,12 @@ var fs = require("fs");
 web = {
 	index : function(req, res, next){
 		res.render('pages/index.ejs',{
-			title:"merdeka",
-
+			title:"YOLO",
+			content: "i'm YOLO, backend engine from nodejs and ExpressJs"
 		});
-
 	},
 	404:function(req,res){
 		res.render('layout/error_template.ejs');
-
-
 	}
 
 }

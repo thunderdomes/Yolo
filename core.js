61b3f705a510a332938d36d1badca91e83781098
Arie <arie@wego.com>
2013-06-25 18:44:59 +0700

    update

--- core.js
+++ core.js
@@ -10,32 +10,31 @@ module.exports = function Core()
 	var mysql = require('mysql');
 
 	var app = express();
-	
 	///setting up
 	app.set('views', __dirname + '/views');
 	app.use(express.static(path.join(__dirname, 'public')));
-    app.engine('ejs', engine);
+	app.engine('ejs', engine);
     app.set('view engine', 'ejs'); // so you can render('index')
-     
+
     var controllers = {};
     for(var route in routes)
-	{
-		var c = routes[route].controller;
-		var a = routes[route].action || 'index';
-		if(typeof controllers[c]  == 'undefined'){
-			controllers[c] = require('./controllers/'+c+'.js');
-		}
-		if(typeof controllers[c]  != 'undefined'){
-			if(typeof controllers[c][a]  == 'undefined'){
-				console.log('Action '+a+' Tidak Ditemukan');
-			}
-			else{
-				app.get(route,controllers[c][a]); 
-			}
-		}
-		else{
-			console.log('Controller '+c+' Tidak Ditemukan');
-		}
-	}
-	app.listen(config.port);
+    {
+    	var c = routes[route].controller;
+    	var a = routes[route].action || 'index';
+    	if(typeof controllers[c]  == 'undefined'){
+    		controllers[c] = require('./controllers/'+c+'.js');
+    	}
+    	if(typeof controllers[c]  != 'undefined'){
+    		if(typeof controllers[c][a]  == 'undefined'){
+    			console.log('Action '+a+' Tidak Ditemukan');
+    		}
+    		else{
+    			app.get(route,controllers[c][a]); 
+    		}
+    	}
+    	else{
+    		console.log('Controller '+c+' Tidak Ditemukan');
+    	}
+    }
+    app.listen(config.port);
 }

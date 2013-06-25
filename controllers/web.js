f5893a240c354de922e98eb951edd73e624a2adb
Arie <arie@wego.com>
2013-06-25 18:56:56 +0700

    update scheme

--- controllers/web.js
+++ controllers/web.js
@@ -1,18 +1,11 @@
 var fs = require("fs");
-var marked = require("marked"); // https://github.com/chjj/marked
 web = {
 	index : function(req, res, next){
-		fs.readFile("./content/tes.md", "ascii", function (err, data) {
-			if (err) {
-				throw err;
-			}
-			res.render('pages/index.ejs',{
-				title:"merdeka",
-				content:marked(data),
+		res.render('pages/index.ejs',{
+			title:"merdeka",
 
-		 });
 		});
-		 
+
 	},
 	404:function(req,res){
 		res.render('layout/error_template.ejs');
